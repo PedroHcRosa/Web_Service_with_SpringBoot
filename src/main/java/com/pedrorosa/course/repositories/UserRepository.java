d3ca26370a8776bd9fa736750396bd482b91f944
Pedro Rosa <opedrorosa@hotmail.com>
2023-09-19 15:40:05 -0300

    Repository and DB Seeding

---
+++ src/main/java/com/pedrorosa/course/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.pedrorosa.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.pedrorosa.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	
+	
+	
+	
+}
